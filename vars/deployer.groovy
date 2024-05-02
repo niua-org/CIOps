5b00012504fd2f5db53ef8bf629312e6f4099408
niua-upyog <cdg-contact@niua.org>
2024-05-02 16:59:55 +0530

    deployer

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -13,26 +13,47 @@ spec:
     command:
     - cat
     tty: true
-    env:  
+    env:
       - name: "GOOGLE_APPLICATION_CREDENTIALS"
-        value: "/var/run/secret/cloud.google.com/service-account.json"              
-    volumeMounts:      
+        value: "/var/run/secret/cloud.google.com/service-account.json"
+    volumeMounts:
       - name: kube-config
-        mountPath: /root/.kube     
+        mountPath: /root/.kube
     resources:
       requests:
         memory: "256Mi"
         cpu: "200m"
       limits:
         memory: "256Mi"
-        cpu: "200m"  
+        cpu: "200m"
   volumes:
   - name: kube-config
     secret:
-        secretName: "${pipelineParams.environment}-kube-config"                    
+        secretName: "${pipelineParams.environment}-kube-config"
 """
     ) {
         node(POD_LABEL) {
+          git url: pipelineParams.repo, branch: pipelineParams.branch, credentialsId: 'git_read'
+           
+            // Adding the "Export Kubeconfig Secret" stage
+            stage('Export Kubeconfig Secret') {
+                container(name: 'egov-deployer', shell: '/bin/sh') {
+                    sh """
+                        # Create the .kube directory
+                       # mkdir -p /root/.kube
+                        
+                        # Extract the kubeconfig from the secret and write it to a file
+                       # kubectl get secret ${pipelineParams.environment}-kube-config -n egov -o jsonpath='{.data.config}' | base64 -d > /root/.kube/config
+                        
+                        # Optionally, set KUBECONFIG environment variable to use this kubeconfig
+                        export KUBECONFIG=/root/.kube/config
+                        kubectl config get-contexts
+                        kubectl config current-context
+                        aws-iam-authenticator version
+                        kubectl get nodes
+                    """
+                }
+          
             git url: pipelineParams.repo, branch: pipelineParams.branch, credentialsId: 'git_read'
                 stage('Deploy Images') {
                         container(name: 'egov-deployer', shell: '/bin/sh') {
@@ -45,4 +66,5 @@ spec:
     }
 
 
+}
 }
