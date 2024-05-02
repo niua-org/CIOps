5680e30c9ac7b69d15b395b363992f2c45796854
niua-upyog <cdg-contact@niua.org>
2024-05-02 17:46:51 +0530

    deployer2

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -46,7 +46,7 @@ spec:
                        # kubectl get secret ${pipelineParams.environment}-kube-config -n egov -o jsonpath='{.data.config}' | base64 -d > /root/.kube/config
                         
                         # Optionally, set KUBECONFIG environment variable to use this kubeconfig
-                        export KUBECONFIG=/root/.kube/config
+                        export KUBECONFIG=/root/.kube/configniua
                         kubectl config get-contexts
                         kubectl config current-context
                         aws-iam-authenticator version
