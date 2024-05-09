440c40739b02b978c75798bbf33701b7b6466cf6
niua-upyog <cdg-contact@niua.org>
2024-05-09 13:12:19 +0530

    branch

--- vars/jobBuilder.groovy
+++ vars/jobBuilder.groovy
@@ -93,7 +93,7 @@ spec:
                         description('') 
                         branch('')      
                         useRepository('')                     
-                        defaultValue('origin/master') 
+                        defaultValue('origin/niua-dev') 
                         branchFilter('.*')
                         tagFilter('*')
                         sortMode('ASCENDING_SMART')
